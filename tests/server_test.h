d3c3dd9d214d3897551fcbd96b80683bbf30bad6
Tulenenok <small-chinchilla@yandex.ru>
2022-11-27 00:28:14 +0300

    Добавила тесты

---
+++ tests/server_test.h
@@ -0,0 +1,13 @@
+//
+// Created by Nataliia Gurova on 26.11.2022.
+//
+
+#ifndef C_VERSION_SERVER_TEST_H
+#define C_VERSION_SERVER_TEST_H
+
+#include "../server/screen_t.h"
+
+int server_test_set_default_color(screen_t *screen);
+int server_test_draw_triangle(screen_t *screen);
+
+#endif //C_VERSION_SERVER_TEST_H
--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -6,16 +6,7 @@
 #include "mainwindow.h"
 #include "ui_MainWindow.h"
 
-#include "../server/screen_t.h"
-#include "../server/screen_properties_t.h"
-#include "../server/triangle_t.h"
-#include "../server/z_buffer.h"
-
-int my_random(int a,int b)
-{
-    if (a > 0) return a + rand() % (b - a);
-    else return a + rand() % (abs(a) + b);
-}
+#include "../tests/server_test.h"
 
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
@@ -30,20 +21,28 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     connect(ui->DrawButton, SIGNAL (clicked()),this, SLOT(on_DrawButton_clicked()));
     connect(ui->AddLightButton, SIGNAL (clicked()),this, SLOT(on_AddLightButton_clicked()));
 
+    screen_properties_t screenProperties {ui->graphicsView->width(), ui->graphicsView->height()};
+    screen_matrix = allocate_screen_t(&screenProperties);
+
 //    ui->graphicsView->setStyleSheet("background-color: black");
 }
 
 MainWindow::~MainWindow() {
+    free_screen_t(screen_matrix);
     delete ui;
 }
 
 void MainWindow::on_LoadButton_clicked() {
-    fillScreen(NULL);
+    server_test_set_default_color(screen_matrix);
+    fillScreen();
 
     handleButton(LOAD);
 }
 
 void MainWindow::on_DrawButton_clicked() {
+    server_test_draw_triangle(screen_matrix);
+    fillScreen();
+
     handleButton(DRAW);
 }
 
@@ -51,85 +50,20 @@ void MainWindow::on_AddLightButton_clicked() {
     handleButton(ADD_LIGHT);
 }
 
-//void MainWindow::fillScreen(color_t **matrix_color)
-//{
-//    int w = ui->graphicsView->width();
-//    int h = ui->graphicsView->height();
-//
-//    QImage image(w, h, QImage::Format_ARGB32 );
-//    for(int y = 0; y < h; ++y)
-//        for(int x = 0; x < w ;x++)
-//        {
-//            color_t color_pix = matrix_color[x][y];
-//            QRgb argb = qRgb(color_pix.r,color_pix.g,color_pix.b);
-//            image.setPixel(QPoint(x, y), argb);
-//        }
-//    ui->graphicsView->scene()->addPixmap(QPixmap::fromImage(image));
-//}
-
-//// TEST 1
-//void MainWindow::fillScreen(int **matrix_color)
-//{
-//    int w = ui->graphicsView->width();
-//    int h = ui->graphicsView->height();
-//
-//    screen_properties_t screenProperties {w, h};
-//
-//    screen_t *screen = allocate_screen_t(&screenProperties);
-//    color_t color = {150, 128, 90};
-//    set_default_color(screen, &color);
-//
-//    color_t **mat = screen->color;
-//
-//    QImage image(w, h, QImage::Format_ARGB32 );
-//    for(int y = 0; y < h; ++y)
-//        for(int x = 0; x < w ;x++)
-//        {
-//            color_t color_pix = mat[x][y];
-//            QRgb argb = qRgb(color_pix.r,color_pix.g,color_pix.b);
-//            image.setPixel(QPoint(x, y), argb);
-//        }
-//    ui->graphicsView->scene()->addPixmap(QPixmap::fromImage(image));
-//
-//    free_screen_t(screen);
-//}
-
-// TEST 2
-void MainWindow::fillScreen(int **matrix_color)
+void MainWindow::fillScreen()
 {
-    int w = ui->graphicsView->width();
-    int h = ui->graphicsView->height();
-
-    std::cout << w << " " << h;
-
-    screen_properties_t screenProperties {w, h};
-    screen_t *screen = allocate_screen_t(&screenProperties);
-
-    std::vector<triangle_t*> triangles;
-    triangle_t *tr1 = allocate_triangle_t();
-
-    set_vertex_by_index(tr1, 0, 0, 0, 0);
-    set_vertex_by_index(tr1, 1, 0, 520, 0);
-    set_vertex_by_index(tr1, 2, 200, 100, 0);
-
-
-    color_t color = {243, 50, 18};
-    set_color(tr1, &color);
-    triangles.push_back(tr1);
-
-    z_buffer_render(screen, triangles);
-
-    color_t **mat = screen->color;
+    int w = screen_matrix->width;
+    int h = screen_matrix->height;
 
     QImage image(w, h, QImage::Format_ARGB32 );
     for(int y = 0; y < h; ++y)
-        for(int x = 0; x < w ;x++)
-        {
-            color_t color_pix = mat[x][y];
-            QRgb argb = qRgb(color_pix.r,color_pix.g,color_pix.b);
-            image.setPixel(QPoint(x, y), argb);
-        }
-    ui->graphicsView->scene()->addPixmap(QPixmap::fromImage(image));
+        for(int x = 0; x < w; ++x)
+            if (screen_matrix->change[x][y])
+            {
+                color_t color_pix = screen_matrix->color[x][y];
+                QRgb argb = qRgb(color_pix.r,color_pix.g,color_pix.b);
+                image.setPixel(QPoint(x, y), argb);
+            }
 
-    free_screen_t(screen);
+    ui->graphicsView->scene()->addPixmap(QPixmap::fromImage(image));
 }
